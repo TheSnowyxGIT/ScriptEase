89b359e63cb57cf05420da75268ac45e168864af
Adrien Pingard <adrien.pingard@gmail.com>
2023-10-06 02:11:09 +0200

    feat(bestmatch): add bestMatch

--- src/errors/MissingScriptError.ts
+++ src/errors/MissingScriptError.ts
@@ -1,26 +1,30 @@
 import * as chalk from 'chalk';
 import SE_ROOT from '../Tree/Root';
 import SE_ERROR from './SeError';
+import didYouMean from 'didyoumean2';
+import NodeManager from 'src/Tree/NodeManager';
 
 export default class SE_MissingScriptError extends SE_ERROR {
   public name = 'SE_MissingScriptError';
   private _identifier: string;
-  private _root: SE_ROOT;
+  private _bestMatch: string | null;
 
-  constructor(identifier: string, root: SE_ROOT) {
+  constructor(identifier: string, bestMatch: string | null) {
     super();
     this._identifier = identifier;
-    this._root = root;
+    this._bestMatch = bestMatch;
   }
 
   private get bestMatch() {
-    return 'TODO';
+    return this._bestMatch;
   }
 
   toString() {
     let text = chalk.gray("It seems that the script you want to execute does not exists : '");
     text += chalk.white(this._identifier) + chalk.gray("'");
-    text += `\n\t${chalk.gray(`Did you mean '${chalk.white(this.bestMatch)}'`)}`;
+    if (this.bestMatch) {
+      text += `\n\t${chalk.gray(`Did you mean '${chalk.white(this.bestMatch)}'`)}`;
+    }
     return text;
   }
 }
